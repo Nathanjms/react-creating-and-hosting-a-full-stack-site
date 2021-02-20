e6ad234d75dab6fbfa6ffd4a0e2e2397d92eb425
Nathan James <nathanjms@outlook.com>
2021-02-20 20:52:56 +0000

    install body-parser and update serve.js

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,7 +1,11 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello'));
+app.use(bodyParser.json());
 
-app.listen(8000, () => console.log('Listeniong on port 8000'));
+app.get('/hello', (req, res) => res.send('Hello'));
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+
+app.listen(8000, () => console.log('Listening on port 8000'));
