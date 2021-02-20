561d5e0adae47f17693df140c04fe3ace4fbfc6b
Nathan James <nathanjms@outlook.com>
2021-02-20 16:17:02 +0000

    Nav bar, link to articles

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import articleContent from './article-content';
+
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+
+    if (!article) return <h1>Article Does Not Exist!</h1>
+
+    return (
+    <React.Fragment>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, key) => (
+            <p key={key}>{paragraph}</p>
+        ))}
+    </React.Fragment>
+    );
+}
+
+export default ArticlePage;
