0064be692498155547e6f547e73f83e4b9eec22f
Nathan James <nathanjms@outlook.com>
2021-02-21 14:32:22 +0000

    Set up mongodb on npm

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,26 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-// "npx nodemon --exec npx babel-node src/server.js" to restart
-
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
-    }
-}
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('my-blog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: "learn-react" });
+        console.log(articleInfo);
+
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+
+})
 
 app.post('/api/articles/(:name)/upvote', (req, res) => {
     const articleName = req.params.name;
@@ -35,7 +38,6 @@ app.post('/api/articles/(:name)/add-comment', (req, res) => {
     const articleName = req.params.name;
 
     articlesInfo[articleName].comments.push({ username, text });
-
     res.status(200).send(articlesInfo[articleName]);
 });
 
