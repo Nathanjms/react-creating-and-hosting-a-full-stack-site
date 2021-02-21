cd2ce54213a929bf3787a0b277a4f38fe30a9f52
Nathan James <nathanjms@outlook.com>
2021-02-21 18:01:00 +0000

    useEffect and useState

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    if (!article) return <h1>Article Does Not Exist!</h1>
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+    }, [name]);
+
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
-    <React.Fragment>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h2>Other Articles:</h2>
-        <ArticlesList articles={ otherArticles } />
-    </React.Fragment>
+        <React.Fragment>
+            <h1>{article.title}</h1>
+            <p>This post has been upvotes {articleInfo.upvotes} times.</p>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+            <h2>Other Articles:</h2>
+            <ArticlesList articles={otherArticles} />
+        </React.Fragment>
     );
 }
 
